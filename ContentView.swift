4c141a4ea9d9a1f41c59981ada39c49c320fd4f1
AK <kingdodgemangay@gmail.com>
2026-04-13 11:09:28 -0500

    fix

--- ContentView.swift
+++ ContentView.swift
@@ -1,8 +1,18 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var chanace = 0
+    @State var answer = ""
     var body: some View {
        Text("Adam Kolacia")
        Text ("Louie")
+        Button {
+            
+        } label: {
+            Image("8ball 2")
+                .frame(width: 300, height: 300)
+        }
+
+        }
     }
-}
+
